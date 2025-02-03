01cab4a23aa048e2fcfe59609f5f7249c6dab58f
AQPRenzo <renzo.gnu@gmail.com>
2025-02-03 17:21:34 -0500

    Inicio

---
+++ src/test/java/com/app/app/AppTest.java
@@ -0,0 +1,16 @@
+package com.app.app;
+
+public class AppTest {
+
+    public AppTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        App.main(args);
+
+    }
+
+}
